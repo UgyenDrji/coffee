3a6c24673ccde6c34caa300a5b655f9bf8022cd6
Anju6770 <anjalighalleyrai@gmail.com>
2024-08-21 21:50:43 +0600

    initial commit

---
+++ lib/data/name_list.dart
@@ -0,0 +1,14 @@
+import 'package:coffee_project/model/coffe_name.dart';
+
+List<CoffeeName> name=[
+  CoffeeName(name: "All Coffees"),
+  CoffeeName(name: "Cappuccino"),
+  CoffeeName(name: "Macchiato"),
+  CoffeeName(name: "Espresso"),
+  CoffeeName(name: "Latte"),
+  CoffeeName(name: "Flat white"),
+  CoffeeName(name: "Affogato"),
+  CoffeeName(name: "Cortado"),
+  CoffeeName(name: "Americano"),
+];
+
